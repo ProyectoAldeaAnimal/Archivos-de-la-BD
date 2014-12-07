c4ef65b85759099ceb3d21f728702bd7c0ad3f0d
Matiasqj <mat.quinteros@outlook.com>
2014-12-07 18:18:39 -0300

    Actualizados Modelo fisico, Triggers, script

--- Triggers/TR_ESTERI_INTERV.sql
+++ Triggers/TR_ESTERI_INTERV.sql
@@ -8,28 +8,28 @@ DECLARE TIPO INT;
 DECLARE MASCOTA INT;
 DECLARE Y INT;
 DECLARE PRESTACION INT;
-SET INTERVENCION = (SELECT ID_INT FROM inter WHERE ID_INT IN (SELECT MAX(ID_INT) FROM inter));
+SET INTERVENCION = (SELECT ID_INT FROM inter WHERE ID_INT=NEW.ID_INT);
 SET OBS = (SELECT OBS_INT FROM inter WHERE ID_INT = INTERVENCION);
 SET FECHA = (SELECT DATE(FECHA_INGRESO_INT) FROM inter WHERE ID_INT =INTERVENCION);
 SET ORDEN = (SELECT ID_ORDEN_INT FROM inter WHERE ID_INT = INTERVENCION);
 SET MASCOTA = (
-	SELECT tm.ID_TIPO_MAS 
-	FROM orden_int as o,atencion as a, mas as m, tipo_mas as tm 
-	WHERE  o.ID_ORDEN_INT = ORDEN and o.ID_ATENCION = a.ID_ATENCION and a.ID_MAS = m.ID_MAS and m.ID_TIPO_MAS = tm.ID_TIPO_MAS
-	LIMIT 1);
+  SELECT tm.ID_TIPO_MAS 
+  FROM orden_int as o,atencion as a, mas as m, tipo_mas as tm 
+  WHERE  o.ID_ORDEN_INT = ORDEN and o.ID_ATENCION = a.ID_ATENCION and a.ID_MAS = m.ID_MAS and m.ID_TIPO_MAS = tm.ID_TIPO_MAS
+  LIMIT 1);
     SET PRESTACION =(
-	SELECT tp.ID_TIPO_PRES 
-	FROM orden_int as oi,atencion as a,pres as p ,tipo_pres as tp
-	WHERE  oi.ID_ORDEN_INT = ORDEN and oi.ID_ATENCION= a.ID_ATENCION and a.ID_PRES = p.ID_PRES and p.ID_TIPO_PRES = tp.ID_TIPO_PRES 
-	LIMIT 1);
+  SELECT tp.ID_TIPO_PRES 
+  FROM orden_int as oi,atencion as a,pres as p ,tipo_pres as tp
+  WHERE  oi.ID_ORDEN_INT = ORDEN and oi.ID_ATENCION= a.ID_ATENCION and a.ID_PRES = p.ID_PRES and p.ID_TIPO_PRES = tp.ID_TIPO_PRES 
+  LIMIT 1);
 SET TIPO = (SELECT ID_TIPO_INT FROM orden_int WHERE ID_ORDEN_INT = ORDEN);
 IF((MASCOTA=1 or MASCOTA =2)and (TIPO=1 or TIPO=2))
 THEN
 
 SET Y = (SELECT mas.ID_MAS 
     FROM orden_int, inter,mas, atencion 
-	WHERE 
-    inter.ID_INT IN (SELECT ID_INT FROM inter WHERE ID_INT IN (SELECT MAX(ID_INT) FROM inter))
+  WHERE 
+    inter.ID_INT IN (SELECT ID_INT FROM inter WHERE ID_INT= NEW.ID_INT)
                                           and inter.ID_ORDEN_INT = orden_int.ID_ORDEN_INT 
                                           and orden_int.ID_ATENCION = atencion.ID_ATENCION and 
                                           atencion.ID_MAS = mas.ID_MAS);
@@ -38,8 +38,8 @@ ELSEIF (PRESTACION=2)
 THEN 
 SET Y = (SELECT mas.ID_MAS 
     FROM orden_int, inter,mas, atencion 
-	WHERE 
-    inter.ID_INT IN (SELECT ID_INT FROM inter WHERE ID_INT IN (SELECT MAX(ID_INT) FROM inter))
+  WHERE 
+    inter.ID_INT IN (SELECT ID_INT FROM inter WHERE ID_INT =NEW.ID_INT)
                                           and inter.ID_ORDEN_INT = orden_int.ID_ORDEN_INT 
                                           and orden_int.ID_ATENCION = atencion.ID_ATENCION and 
                                           atencion.ID_MAS = mas.ID_MAS);
